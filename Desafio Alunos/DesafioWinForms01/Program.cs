d4d03b89ae4a3f0f308c18c43dddfc129865d2bf
unknown <jfs102019@hotmailcom>
2023-06-05 17:08:32 -0300

    Meu comentário bonito

---
+++ Desafio Alunos/DesafioWinForms01/Program.cs
@@ -0,0 +1,14 @@
+namespace DesafioWinForms01
+{
+    internal static class Program
+    {
+        
+        [STAThread]
+        static void Main()
+        {
+             https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Lista());
+        }
+    }
+}
